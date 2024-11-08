bd6532782b2c1140e794443f18dee8413eb18fa4
Benja <benjaminvilla409@gmail.com>
2024-11-07 21:43:57 -0300

    cree las demás acciones (prueba más que nada)

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -1,15 +1,35 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using ProductoRepositoryNamespace;
+using TiendaNamespace;
 using tl2_tp6_2024_Daggam.Models;
 
 namespace tl2_tp6_2024_Daggam.Controllers;
 
 public class ProductosController : Controller
 {
+    ProductoRepository productoRepository;
+
+    public ProductosController()
+    {
+        productoRepository = new SQLiteProductoRepository();
+    }    
     public IActionResult Index(){
         return View();
     }
     public IActionResult Listar()
+    {
+        List<Producto>? productos = productoRepository.obtenerProductos();
+        return View(productos);
+    }
+    public IActionResult Crear()
+    {
+        return View();
+    }
+    public IActionResult Modificar()
+    {
+        return View();
+    }
+    public IActionResult Eliminar()
     {
         return View();
     }
