4f0d86f4ce2a8bf96300d7143086b302df156eec
Benja <benjaminvilla409@gmail.com>
2024-11-23 02:25:28 -0300

    Termine de agregar las validaciones

--- ViewModel/LoginViewModel.cs
+++ ViewModel/LoginViewModel.cs
@@ -1,5 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ViewModel;
 public class LoginViewModel{
+    [Required(ErrorMessage = "El campo usuario es requerido")]
     public string Usuario{get;set;}
+    [Required(ErrorMessage = "El campo contraseña es requerido")]
     public string Password{get;set;}
 }
